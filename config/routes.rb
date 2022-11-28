13b816b64bd732701c178744c66eb21519133407
ana castaneda <anita_1017@hotmail.com>
2022-11-28 15:38:22 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+  resources :profiles
+  resources :matches, only: %i[index show create]
+  resources :chatrooms, only: %i[index show] do
+    resources :messages, only: %i[create]
+  end
+  resources :meetings, only: %i[index new create update]
+  # resources :likes, only: %i[update destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
